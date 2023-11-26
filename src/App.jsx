ff64ce62badf13ae7278bbb2269ee6752c25f386
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-26 21:27:31 +0600

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h2>Welcome to peoples forum client</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,19 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            This is home page
+
+            <div>
+                <button className="btn btn-info">Info</button>
+                <button className="btn btn-success">Success</button>
+                <button className="btn btn-warning">Warning</button>
+                <button className="btn btn-error">Error</button>
+            </div>
+
+        </div>
+    );
+};
+
+export default Home;
