e2dde4dda1a10e9018bc4616fa64319d6d1a31ed
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-27 20:48:16 +0600

    Modify post cards and add sort button

--- src/pages/Home/Posts/Post.jsx
+++ src/pages/Home/Posts/Post.jsx
@@ -1,6 +1,7 @@
 import Tag from "./Tag";
 import { FaComment } from "react-icons/fa";
 import { FaVoteYea } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 
 const Post = ({ post }) => {
@@ -14,35 +15,35 @@ const Post = ({ post }) => {
         tags,
         createdAt,
         updatedAt,
-        upvote,
-        downvote,
-        voteDifference
+        upVote,
+        downVote
     } = post;
 
     return (
-        <div className="card card-side bg-base-100 shadow-xl">
-            <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">{title}</h2>
 
-                <div className="">
-                    <div className="flex items-center justify-center gap-2 bg-teal-200 p-2 rounded-lg">
-                        <FaComment></FaComment> 60
+        <Link to={`post/${_id}`}>
+            <div className="card card-side bg-base-100 shadow-xl">
+                <figure className="ml-3"><img className="rounded-xl" src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{title}</h2>
+                    <div className="flex flex-wrap gap-2">
+                        {
+                            tags.map((tag, idx) => <Tag key={idx} tag={tag}></Tag>)
+                        }
                     </div>
-
-                    <div className="flex items-center justify-center gap-2 bg-amber-300 p-2 rounded-xl">
-                        <FaVoteYea></FaVoteYea> {voteDifference}
+                    <div className="card-actions justify-start">
+                        <div className="flex flex-row gap-5">
+                            <div className="flex items-center justify-center gap-2 bg-teal-200 p-3 rounded-lg">
+                                <FaComment className="text-2xl"></FaComment> 60
+                            </div>
+                            <div className="flex items-center justify-center gap-2 bg-amber-300 p-3 rounded-lg">
+                                <FaVoteYea className="text-2xl"></FaVoteYea> {upVote - downVote}
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                {
-                    tags.map((tag, idx) => <Tag key={idx} tag={tag}></Tag>)
-                }
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Watch</button>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
--- src/pages/Home/Posts/Posts.jsx
+++ src/pages/Home/Posts/Posts.jsx
@@ -1,21 +1,31 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Post from "./Post";
-
+import { FaSortAmountUp } from "react-icons/fa";
 
 const Posts = () => {
 
     const [posts, setPosts] = useState([]);
+    const [sort, setSort] = useState(0);
 
     useEffect(() => {
-        axios.get('posts.json')
+        axios.get(`http://localhost:5000/posts?sort=${sort}`)
             .then(res => {
                 setPosts(res.data);
             }).catch(err => console.log(err));
-    }, [])
+    }, [sort])
 
+    const handleSort = () => {
+        setSort(1);
+    }
+    
     return (
         <div>
+            <div className="flex justify-end mb-5">
+                {
+                    <button disabled={sort} onClick={handleSort} className="btn btn-outline ">  Sort by Popularity <FaSortAmountUp></FaSortAmountUp></button>
+                }
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2  gap-5">
                 {posts.map((post) => <Post key={post._id} post={post}></Post>)}
             </div>
