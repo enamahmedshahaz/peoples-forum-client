7069af379565ef27c14702cc07c381405b295327
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-28 21:43:21 +0600

    Create a provider  AuthProvider for auth related tasks

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,19 @@ import './index.css'
 import { RouterProvider } from "react-router-dom";
 import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './providers/AuthProvider';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-6xl mx-auto'>
-      <HelmetProvider>
-        <RouterProvider router={router} />
-      </HelmetProvider>
-    </div>
+    
+    <AuthProvider>
+      <div className='max-w-6xl mx-auto'>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </div>
+    </AuthProvider>
+
   </React.StrictMode>,
 )
